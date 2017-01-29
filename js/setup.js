e23dec76a6d0a63eee119e5270e10ddb1637600b
“Enclave88git config --global user.email “sutangu@gmail.com <sutangu@gmail.com>
2017-01-29 20:33:02 +0600

    #10 Учебный проект: надеть Одежду

---
+++ js/setup.js
@@ -0,0 +1,60 @@
+'use strict';
+
+// Menu
+var setup = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = document.querySelector('.setup-close');
+
+// Wizard
+var wizardCoat = document.querySelector('#wizard-coat');
+var wizardEyes = document.querySelector('#wizard-eyes');
+var fireball = document.querySelector('.setup-fireball-wrap');
+
+// Colors
+var wizardCoatColors = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
+var wizardEyesColor = [
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
+];
+var wizardFireballColor = [
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848'
+];
+
+// Menu events
+setupOpen.addEventListener('click', function () {
+  setup.classList.remove('invisible');
+});
+
+setupClose.addEventListener('click', function () {
+  setup.classList.add('invisible');
+});
+
+// Colors events
+wizardCoat.addEventListener('click', function () {
+  var coatColor = Math.floor(Math.random() * wizardCoatColors.length);
+  wizardCoat.style.fill = wizardCoatColors[coatColor];
+});
+
+wizardEyes.addEventListener('click', function () {
+  var eyesColor = Math.floor(Math.random() * wizardEyesColor.length);
+  wizardEyes.style.fill = wizardEyesColor[eyesColor];
+});
+
+fireball.addEventListener('click', function () {
+  var fireballColor = Math.floor(Math.random() * wizardFireballColor.length);
+  fireball.style.background = wizardFireballColor[fireballColor];
+});
