d470ed7298aeeb7b968beda3c8f8938d8fe03378
“Enclave88git config --global user.email “sutangu@gmail.com <sutangu@gmail.com>
2017-02-06 00:32:18 +0600

    more ARIA attributes and hotkeys

--- js/setup.js
+++ js/setup.js
@@ -5,6 +5,7 @@ var setup = document.querySelector('.setup');
 var setupOpen = document.querySelector('.setup-open');
 var setupOpenIcon = document.querySelector('.setup-open-icon');
 var setupClose = document.querySelector('.setup-close');
+var setupSubmit = document.querySelector('.setup-submit');
 
 // Wizard
 var wizardCoat = document.querySelector('#wizard-coat');
@@ -35,13 +36,71 @@ var wizardFireballColor = [
   '#e6e848'
 ];
 
-// Menu events
-setupOpen.addEventListener('click', function () {
+// Constants
+var ENTER_KEY_CODE = 13;
+var ESCAPE_KEY_CODE = 27;
+
+var getSetupOpenButtonAriaPressed = function () {
+  return setupOpenIcon.setAttribute('aria-pressed', 'false');
+};
+var setSetupOpenButtonAriaPressed = function () {
+  return setupOpenIcon.setAttribute('aria-pressed', 'true');
+};
+
+var getSetupCloseButtonAriaPressed = function () {
+  return setupClose.setAttribute('aria-pressed', 'false');
+};
+var setSetupCloseButtonAriaPressed = function () {
+  return setupClose.setAttribute('aria-pressed', 'true');
+};
+
+var getSetupSubmitButtonAriaPressed = function () {
+  return setupSubmit.setAttribute('aria-pressed', 'false');
+};
+var setSetupSubmitButtonAriaPressed = function () {
+  return setupSubmit.setAttribute('aria-pressed', 'true');
+};
+
+var getInvisibleClass = function () {
   setup.classList.remove('invisible');
+  setSetupOpenButtonAriaPressed();
+  getSetupCloseButtonAriaPressed();
+  getSetupSubmitButtonAriaPressed();
+};
+
+var setInvisibleClass = function () {
+  setup.classList.add('invisible');
+  getSetupOpenButtonAriaPressed();
+  setSetupCloseButtonAriaPressed();
+};
+
+// Menu events
+setupOpen.addEventListener('click', getInvisibleClass);
+
+setupOpenIcon.addEventListener('keydown', function (e) {
+  if (e.keyCode && e.keyCode === ENTER_KEY_CODE) {
+    getInvisibleClass();
+    document.addEventListener('keydown', function (e) {
+      if (e.keyCode && e.keyCode === ESCAPE_KEY_CODE) {
+        setInvisibleClass();
+      }
+    });
+  }
 });
 
-setupClose.addEventListener('click', function () {
-  setup.classList.add('invisible');
+setupClose.addEventListener('click', setInvisibleClass);
+
+setupClose.addEventListener('keydown', function (e) {
+  if (e.keyCode && e.keyCode === ENTER_KEY_CODE) {
+    setInvisibleClass();
+  }
+});
+
+setupSubmit.addEventListener('keydown', function (e) {
+  if (e.keyCode && e.keyCode === ENTER_KEY_CODE) {
+    getInvisibleClass();
+    setSetupSubmitButtonAriaPressed();
+  }
 });
 
 // Colors events
@@ -61,12 +120,17 @@ fireball.addEventListener('click', function () {
 });
 
 function setHtmlValues() {
-  // ARIA roles
+  // ARIA roles and other attr
   setup.setAttribute('role', 'dialog');
   setupOpenIcon.setAttribute('role', 'button');
+  getSetupOpenButtonAriaPressed();
   setupOpenIcon.setAttribute('tabindex', '0');
   setupClose.setAttribute('role', 'button');
+  getSetupCloseButtonAriaPressed();
   setupClose.setAttribute('tabindex', '0');
+  setupSubmit.setAttribute('role', 'button');
+  getSetupSubmitButtonAriaPressed();
+  setupSubmit.setAttribute('tabindex', '1');
 }
 
 setHtmlValues();
